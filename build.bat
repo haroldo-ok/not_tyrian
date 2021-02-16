6b9d774ceab6bf06d3ee7d9cdf09104a0dc7774b
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-02-15 21:46:38 -0300

    Use newer bmp2tile.

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
+D:\Util\bmp2tile-0.5\BMP2Tile.exe gfx\png\player.png -noremovedupes -8x16 -savetiles "gfx\player (tiles).psgcompr"
 folder2c gfx gfx
+sdcc -c -mz80 --peep-file peep-rules.txt gfx.c
 sdcc -c -mz80 --peep-file peep-rules.txt not_tyrian.c
-sdcc -o not_tyrian.ihx -mz80 --no-std-crt0 --data-loc 0xC000 crt0_sms.rel not_tyrian.rel SMSlib.lib PSGlib.rel
+sdcc -o not_tyrian.ihx -mz80 --no-std-crt0 --data-loc 0xC000 crt0_sms.rel not_tyrian.rel gfx.rel SMSlib.lib PSGlib.rel
 ihx2sms not_tyrian.ihx not_tyrian.sms
