627e5e672506f81d2f0b66239a27c6b316165cbd
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2021-02-18 19:50:26 -0300

    Add new enemy.

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 D:\Util\bmp2tile-0.5\BMP2Tile.exe gfx\png\player.png -noremovedupes -8x16 -savetiles "gfx\player (tiles).psgcompr"
 D:\Util\bmp2tile-0.5\BMP2Tile.exe gfx\png\u_fighter.png -noremovedupes -8x16 -savetiles "gfx\u_fighter (tiles).psgcompr"
+D:\Util\bmp2tile-0.5\BMP2Tile.exe gfx\png\enemy.png -noremovedupes -8x16 -savetiles "gfx\enemy (tiles).psgcompr"
 folder2c gfx gfx
 sdcc -c -mz80 --peep-file peep-rules.txt gfx.c
 sdcc -c -mz80 --peep-file peep-rules.txt not_tyrian.c
--- not_tyrian.c
+++ not_tyrian.c
@@ -8,6 +8,7 @@
 
 const unsigned char base_tile_indexes[] = { 2, 8, 14, 20, 26 };
 const unsigned char uship_tile_indexes[] = { 64, 70, 76, 82, 88, 94, 100, 106 };
+const unsigned char enemy_tile_indexes[] = { 160, 166, 172, 178, 184, 178, 172, 166, 160 };
 
 void draw_ship(unsigned char x, unsigned char y, unsigned char base_tile, unsigned char line_incr) {
 	SMS_addSprite(x, y, base_tile);
@@ -26,13 +27,16 @@ void main(void) {
 	unsigned char joy = 0;
 	int tilt = 0;
 	unsigned char uship_frame = 0;
+	unsigned char enemy_frame = 0;
 	
 	SMS_useFirstHalfTilesforSprites(true);
 	SMS_setSpriteMode(SPRITEMODE_TALL);
 
+	SMS_setBGPaletteColor(0, 0x04);	
 	SMS_loadSpritePalette(player__palette__bin);
 	SMS_loadPSGaidencompressedTiles(player__tiles__psgcompr, 2);
 	SMS_loadPSGaidencompressedTiles(u_fighter__tiles__psgcompr, 64);
+	SMS_loadPSGaidencompressedTiles(enemy__tiles__psgcompr, 160);
 	SMS_setClippingWindow(0, 0, 255, 192);
 	SMS_displayOn();
 	
@@ -51,14 +55,13 @@ void main(void) {
 		}
 		
 		uship_frame++;
+		enemy_frame++;
+		if ((enemy_frame >> 2) > 8) enemy_frame = 0;
 		
 		SMS_initSprites();
 
-		draw_ship(8, 8, 2, 30);
-		draw_ship(8, 40, 8, 30);
-		draw_ship(8, 72, 14, 30);
-
-		draw_ship(64, 8, uship_tile_indexes[(uship_frame >> 2) & 0x07], 48);
+		draw_ship(48, 8, uship_tile_indexes[(uship_frame >> 2) & 0x07], 48);
+		draw_ship(256 - 48 - 24, 8, enemy_tile_indexes[(enemy_frame >> 2)], 30);
 
 		draw_ship(x, 160, base_tile_indexes[(tilt + (2 << 2)) >> 2], 30);
 
